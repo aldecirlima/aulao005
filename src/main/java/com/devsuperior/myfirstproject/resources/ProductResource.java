4a2347b9d0c427fefb77cd8d60e40539c2ce7763
Aldecir Lima <aldecirlima@gmail.com>
2021-08-11 21:31:07 -0300

    Second Commit

---
+++ src/main/java/com/devsuperior/myfirstproject/resources/ProductResource.java
@@ -0,0 +1,37 @@
+package com.devsuperior.myfirstproject.resources;
+
+import java.util.List;
+
+/*
+ * PAREI EM 1:17:00 1 HORA E 17 MINUTOS
+ * */
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.devsuperior.myfirstproject.entities.Product;
+import com.devsuperior.myfirstproject.repositories.ProductRepository;
+
+@RestController
+@RequestMapping(value = "/products")
+public class ProductResource {
+	
+	@Autowired
+	private ProductRepository categoryRepository;
+
+	@GetMapping
+	public ResponseEntity<List<Product>> findAll() {
+		List<Product> list = categoryRepository.findAll();
+		return ResponseEntity.ok().body(list);
+	}
+
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<Product> findById(@PathVariable Long id) {
+		Product cat = categoryRepository.findById(id);
+		return ResponseEntity.ok().body(cat);
+	}
+}
